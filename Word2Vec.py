bed6337eb71b89fb576f2cf3b06436bddbfa259e
Nishant Sinha <nishant15066@iiitd.ac.in>
2018-11-18 23:20:30 +0530

    Word2Vec Done

--- Word2Vec.py
+++ Word2Vec.py
@@ -1,6 +1,10 @@
 import gensim, time
 
-def word2vec():
-    t = time.time()
-    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-    print("Loading took ", time.time() - t ," seconds.")
+t = time.time()
+model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+print("Loading took ", time.time() - t ," seconds.")
+
+
+print(model.most_similar(positive=['Delhi', 'China'], negative=['India'], topn=10))
+
+print(model.most_similar(positive=['Isro', 'USA'], negative=['India'], topn=10))
